05601d90ea77cf7bc694a465354e639f235815dd
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2025-09-30 09:15:06 -0300

    main
    - mudanças nos arquivos iniciais do code-connect

--- code-connect/src/components/Aside/index.jsx
+++ code-connect/src/components/Aside/index.jsx
@@ -6,11 +6,13 @@ import { IconInfo } from '../icons/IconInfo'
 import { IconLogin } from '../icons/IconLogin'
 import { Button } from '../Button'
 import { Link } from '../Link'
+import { useAuth } from '../../hooks/useAuth' 
 
 import styles from './aside.module.css'
 
 export const Aside = () => {
 
+    const { isAuthenticated } = useAuth() 
 
     return (<aside className={styles.aside}>
         <nav>
@@ -43,12 +45,22 @@ export const Aside = () => {
                         Sobre nós
                     </AsideLink>
                 </li>
-                <li>
-                    <AsideLink href="/auth/logout">
-                        <IconLogin />
-                        Logout
-                    </AsideLink>
-                </li>
+                {isAuthenticated && (
+                    <li>
+                        <AsideLink href="/auth/logout">
+                            <IconLogin />
+                            Logout
+                        </AsideLink>
+                    </li>
+                )}
+                {!isAuthenticated && (
+                    <li>
+                        <AsideLink href="/auth/login">
+                            <IconInfo />
+                            Login
+                        </AsideLink>
+                    </li>
+                )}
             </ul>
         </nav>
     </aside>)
